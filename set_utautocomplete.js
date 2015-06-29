cf3e02642b9e5a01794ba09dd24387a46f2aca01
waltsj19 <waltsj19@gmail.com>
2015-06-29 11:56:55 -0500

    3 times a charm

--- set_utautocomplete.js
+++ set_utautocomplete.js
@@ -190,8 +190,8 @@ jQuery.widget("ui.autocomplete", jQuery.ui.autocomplete, {
         var $blArea = $html.find('#bl_area');
         if ($blArea.length === 0) {
             $blArea = $('<div id="bl_area">').css({
-				'paddingLeft': 15px,
-				'marginBottom': 15px
+				paddingLeft: '15px',
+				marginBottom: '15px'
 			});
             $blArea.appendTo($html);
         }
